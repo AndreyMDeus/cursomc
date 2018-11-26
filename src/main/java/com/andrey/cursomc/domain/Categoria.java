e17784c3defc50d148ce39aa808d7e17c882732a
AndreyMDeus <andrey.martins@yahoo.com.br>
2018-11-26 13:33:32 -0200

    Conexão com Banco de dados H2 e criação da tabela Categorias

--- src/main/java/com/andrey/cursomc/domain/Categoria.java
+++ src/main/java/com/andrey/cursomc/domain/Categoria.java
@@ -2,10 +2,19 @@ package com.andrey.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+//Essa é uma anotação do pacote JPA que foi informado na dependência
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
