c7a5621807bb27dc56482ca9505d54dbda4d3074
AndreyMDeus <andrey.martins@yahoo.com.br>
2018-11-26 14:16:57 -0200

    Criando os pacotes repositories e services para a classe Categoria

---
+++ src/main/java/com/andrey/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.andrey.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andrey.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
--- src/main/java/com/andrey/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andrey/cursomc/resources/CategoriaResource.java
@@ -3,33 +3,28 @@ package com.andrey.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andrey.cursomc.domain.Categoria;
+import com.andrey.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	//Modifiquei o tipo de String para List no metodo
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
 		
-		//Instanciei dois objetos Categoria conforme o diagrama de classes
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		//Instanciei uma interface do tipo lista utilizando a classe ArrayList que implementa a interface List
-		List<Categoria> lista = new ArrayList<>();
-		
-		//Adicionei os objetos à interface lista
-		lista.add(cat1);
-		lista.add(cat2);
-				
-		
-		return lista;
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 
